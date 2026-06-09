f229ca56b0957732004b185e94cb69928d320682
simaemadi <emadi.sima@gmail.com>
2026-06-09 09:47:14 +0330

    About me page

--- client/src/app/routes/AppRouter.tsx
+++ client/src/app/routes/AppRouter.tsx
@@ -6,9 +6,10 @@ import PublicLayout from "../layouts/PublicLayout";
 import HomePage from "../../pages/HomePage";
 import AboutPage from "../../pages/AboutPage";
 import ServicesPage from "../../pages/ServicesPage";
-import BlogPage from "../../pages/BlogPage";
+import ArticlesPage from "../../pages/ArticlesPage";
 import ContactPage from "../../pages/ContactPage";
 import TestApiPage from "../../pages/TestApiPage";
+import ArticleDetailsPage from "../../pages/ArticleDetailsPage";
 
 const AppRouter = () => {
   return (
@@ -18,9 +19,10 @@ const AppRouter = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/services" element={<ServicesPage />} />
-          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/articles" element={<ArticlesPage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/test-api" element={<TestApiPage />} />
+          <Route path="/articles/:slug" element={<ArticleDetailsPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
